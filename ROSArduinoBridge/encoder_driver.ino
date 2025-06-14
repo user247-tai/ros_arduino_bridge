831c45debc99c25d4987848f7ed5ca676233a72c
tai <nexoncolors@gmail.com>
2025-06-14 18:18:02 +0700

    Modify logic to adapt mybot base

--- ROSArduinoBridge/encoder_driver.ino
+++ ROSArduinoBridge/encoder_driver.ino
@@ -54,7 +54,7 @@
   
   /* Wrap the encoder reading function */
   long readEncoder(int i) {
-    if (i == LEFT) return left_enc_pos;
+    if (i == LEFT) return -left_enc_pos;
     else return right_enc_pos;
   }
 
